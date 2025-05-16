dea4c0aa42b934f8c763fa435166a0c70a0e812a
MohammedjaveedA <mohammedjaveed412004@gmail.com>
2025-05-16 14:58:56 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Skills from "./components/skills";
+import Skills from "./components/Skills";
 import Footer from "./components/Footer"
 function App() {
   const homeRef = useRef(null);
